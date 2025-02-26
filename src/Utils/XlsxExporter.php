cf18b02198dfdf451d9a5e763ae7413fd9693e8a
Svetlikova <svetlikovahana@proton.me>
2025-02-26 21:07:39 +0100

    Exports - changes in methods, simplifications and minor fixes, added rows

--- src/Utils/XlsxExporter.php
+++ src/Utils/XlsxExporter.php
@@ -8,39 +8,28 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class XlsxExporter
 {
-    public function export(array $data, array $columns, string $filename): void
+    public function export(array $data, string $filename): void
     {
+        bdump($data);
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
 
-        // Set column headers
-        $columnIndex = 1;
-        foreach ($columns as $header => $method) {
-            // The coordinate is a string like 'A1', 'B1', 'C1', etc.
-            $cellCoordinate = Coordinate::stringFromColumnIndex($columnIndex) . '1';
-            $sheet->setCellValue($cellCoordinate, $header); // Pass the correct coordinate format
-            $columnIndex++;
-        }
-
         // Populate rows with data
-        $rowIndex = 2;
+        $rowIndex = 1;
         foreach ($data as $row) {
             $columnIndex = 1;
 
-            foreach ($columns as $header => $method) {
+            foreach ($row as $column) {
                 $cellCoordinate = Coordinate::stringFromColumnIndex($columnIndex) . $rowIndex;
-                // Use reflection or any method to access data dynamically
-
-                // TODO vypsat data
-                $sheet->setCellValue($cellCoordinate, $row->$method()); // Pass the correct coordinate and value
+                $sheet->setCellValue($cellCoordinate, $column); // Pass the correct coordinate and value
                 $columnIndex++;
             }
             $rowIndex++;
         }
 
         // Write the spreadsheet to a file
-        // TODO: filename
-        $filename = '/tmp/products_export.xlsx';
+        $timestamp = date('Y-m-d');
+        $filename = '/tmp/produkty_export_' . $timestamp . '.xlsx';
         $writer = new Xlsx($spreadsheet);
         $writer->save($filename);
 
