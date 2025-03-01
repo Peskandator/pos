73c98519ea845aa4f7bc03a4ad5613a2f00cd2b6
Svetlikova <svetlikovahana@proton.me>
2025-03-01 19:31:02 +0100

    Exports - added for dining tables

--- src/Presenters/Admin/ExportPresenter.php
+++ src/Presenters/Admin/ExportPresenter.php
@@ -5,12 +5,14 @@ namespace App\Presenters\Admin;
 use App\Presenters\BaseCompanyPresenter;
 use App\Utils\XlsxExporter;
 use App\Product\ORM\CategoryRepository;
+use App\Product\ORM\TableRepository;
 
 class ExportPresenter extends BaseCompanyPresenter
 {
     public function __construct(
         private readonly XlsxExporter       $xlsxExporter,
-        private readonly CategoryRepository $categoryRepository
+        private readonly CategoryRepository $categoryRepository,
+        private readonly TableRepository    $tableRepository
     )
     {
         parent::__construct();
@@ -32,6 +34,15 @@ class ExportPresenter extends BaseCompanyPresenter
         $rows = $this->xlsxExporter->createCategoryDataForExport($category);
 
         $this->xlsxExporter->export($rows, 'kategorie');
+        $this->terminate();
+    }
+
+    public function actionDiningTable(): void
+    {
+        $table = $this->tableRepository->findAll();
+        $rows = $this->xlsxExporter->createTableDataForExport($table);
+
+        $this->xlsxExporter->export($rows, 'stoly');
         $this->terminate();
     }
 }
