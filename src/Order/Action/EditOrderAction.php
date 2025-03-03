04a634fc20b2e2a6ae4add10c061c3eafdbac051
Svetlikova <svetlikovahana@proton.me>
2025-03-03 22:46:45 +0100

    Orders - edits

--- src/Order/Action/EditOrderAction.php
+++ src/Order/Action/EditOrderAction.php
@@ -4,7 +4,9 @@ namespace App\Order\Action;
 
 use App\Entity\Company;
 use App\Entity\Order;
+use App\Entity\OrderItem;
 use App\Order\Requests\CreateOrderRequest;
+use App\Product\ORM\ProductRepository;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -13,12 +15,28 @@ class EditOrderAction
 
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
+        private readonly ProductRepository $productRepository,
     ) {
     }
 
-    public function __invoke(Company $company, Order $order, CreateOrderRequest $request, array $productsInGroup): void
+    public function __invoke(Company $company, Order $order, CreateOrderRequest $request, array $orderItems): void
     {
-        // TODO
+        $order->removeOrderItems();
+        $order->updateFromRequest($request);
+
+        $currentItems = $order->getOrderItems();
+        foreach ($currentItems as $item) {
+            $this->entityManager->remove($item);
+        }
+        $this->entityManager->flush();
+
+        foreach ($orderItems as $orderItem) {
+            $product = $this->productRepository->find($orderItem['product']);
+            $item = new OrderItem($order, $product, $orderItem['quantity']);
+
+            $order->addOrderItem($item);
+            $this->entityManager->persist($item);
+        }
 
 
         $this->entityManager->flush();
