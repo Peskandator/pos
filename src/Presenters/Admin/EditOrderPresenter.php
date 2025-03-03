c080f37bb5fb519ab7b7cfa6cb652ac23947a75a
Svetlikova <svetlikovahana@proton.me>
2025-03-03 21:31:56 +0100

    Orders - add edit order

---
+++ src/Presenters/Admin/EditOrderPresenter.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters\Admin;
+use App\Components\Breadcrumb\BreadcrumbItem;
+use App\Entity\Order;
+use App\Order\ORM\OrderRepository;
+use App\Presenters\BaseCompanyPresenter;
+use App\Order\Forms\OrderFormFactory;
+use Nette\Application\UI\Form;
+
+final class EditOrderPresenter extends BaseCompanyPresenter
+{
+
+    public function __construct(
+        private readonly OrderFormFactory $orderFormFactory,
+        private readonly OrderRepository $orderRepository,
+    )
+    {
+        parent::__construct();
+    }
+
+    public function actionDefault(int $orderId): void
+    {
+        $order = $this->orderRepository->find($orderId);
+
+        $this->getComponent('breadcrumb')->addItem(
+            new BreadcrumbItem(
+                'Správa objednávek',
+                null)
+        );
+        $this->getComponent('breadcrumb')->addItem(
+            new BreadcrumbItem(
+                'Objednávky',
+                $this->lazyLink(':Admin:Orders:default'))
+        );
+        $this->getComponent('breadcrumb')->addItem(
+            new BreadcrumbItem(
+                (string)$order->getInventoryNumber(),
+                null)
+        );
+
+        $this->template->order = $order;
+        $this->template->orderItemOptions = $this->currentCompany->getProducts();
+    }
+
+    protected function createComponentEditOrderForm(): Form
+    {
+        $order = $this->template->order;
+        $form = $this->orderFormFactory->create($this->currentCompany, $order);
+
+        $this->orderFormFactory->fillInForm($form, $order);
+        return $form;
+    }
+}
