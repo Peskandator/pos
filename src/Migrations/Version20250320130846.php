2806d57d016a30b31be4e25169e21a33e0afb409
NoGlow <nogmcpe@gmail.com>
2025-03-20 14:38:10 +0100

    Payments - fix migrations, remove QR route

---
+++ src/Migrations/Version20250320130846.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250320130846 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE payment CHANGE paymentTime payment_time DATETIME NOT NULL, CHANGE paymentMethod payment_method VARCHAR(50) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE payment CHANGE payment_time paymentTime DATETIME NOT NULL, CHANGE payment_method paymentMethod VARCHAR(50) NOT NULL');
+    }
+}
--- src/Router/RouterFactory.php
+++ src/Router/RouterFactory.php
@@ -23,8 +23,6 @@ final class RouterFactory
         ]);
         $router->addRoute('<presenter>/<action>[/<id>]', 'Home:default');
 
-        $router->addRoute('admin/order-payment/generate-qr-code', 'Admin:OrderPayment:generateQrCode');
-
         return $router;
 	}
 }
